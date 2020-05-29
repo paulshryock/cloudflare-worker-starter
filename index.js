6964a92672c4b59cdb6b1d58abbcffdb771f0724
Paul Shryock <paul@pshry.com>
2020-05-29 15:34:27 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Respond to the request
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+
+  // Define allowed domain origins
+  const allowed = [
+    'https://newprojectstarterkit.com',
+    'https://pshry.com'
+  ]
+
+  // Define HTTP headers
+  const headers = new Headers({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
+    'Access-Control-Allow-Headers': '*'
+  })
+
+  // If domain is not allowed, return error code
+  if (!allowed.includes(request.headers.get('origin'))) {
+    return new Response('Not allowed', { status: 403, headers: headers });
+  }
+
+  // Define response
+  const response = {
+    greeting: 'hello world'
+  }
+
+  // Return response
+  return new Response(JSON.stringify(response), { status: 200, headers: headers })
+}
